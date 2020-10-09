6ec37b6408030c377674d4304e28212a2e1774c5
Pushpender Rana <pushpender661@gmail.com>
2020-10-09 23:45:11 +0530

    Add version tag to builds

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,4 @@
 !#/bin/sh
-export TAG_NAME=`git rev-parse HEAD`
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
 docker build -t ranapushpender/docker-flutter-build:latest .
 docker build -t ranapushpender/docker-flutter-build:$TAG_NAME .
